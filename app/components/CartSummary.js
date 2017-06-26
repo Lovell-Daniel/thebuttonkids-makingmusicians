185c3bb679226d99a38184352d9df60996cf6037
Daniel Lovell <daniel.lovell@me.com>
2017-06-26 11:02:09 -0700

    add subtotal to cart summary

--- app/components/CartSummary.js
+++ app/components/CartSummary.js
@@ -4,16 +4,19 @@ import React from 'react'
 import {calculateShipping} from '../utils/cart.js'
 
 export default function(props) {
+  let subtotal = 0
   let shipping = 0
   let total = 0
 
   if (props.cartItems && props.cartItems.data) {
+    subtotal = props.cartItems.meta.display_price.without_tax.amount/100
     shipping = calculateShipping(props.cartItems.data)
-    total = shipping + props.cartItems.meta.display_price.without_tax.amount/100
+    total = shipping + subtotal
   }
 
   return (
     <div>
+      <div>Subtotal: ${subtotal.toFixed(2)}</div>
       <div>Shipping and Handling: ${shipping.toFixed(2)}</div>
       <div>Total: ${total.toFixed(2)}</div>
     </div>
